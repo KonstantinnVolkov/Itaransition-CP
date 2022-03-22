c1ebc1a4ca6279a6a7cead1f69feb23546e4a2a6
Ypq5701z <volkovkonstantinn@gmail.com>
2022-03-22 11:32:22 +0300

    empty commit

--- src/main/java/com/example/deploy/controllers/FeedContoller.java
+++ src/main/java/com/example/deploy/controllers/FeedContoller.java
@@ -1,5 +1,8 @@
 package com.example.deploy.controllers;
 
+import com.example.deploy.models.Comment;
+import com.example.deploy.models.Post;
+import com.example.deploy.repositories.CommentRepository;
 import com.example.deploy.repositories.PostRepository;
 import com.example.deploy.repositories.UserRepository;
 import com.example.deploy.services.PostService;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class FeedContoller {
@@ -17,14 +21,19 @@ public class FeedContoller {
     private final UserRepository userRepository;
     private final PostService postService;
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     @Autowired
     public FeedContoller(PostService postService, PostRepository postRepository,
-                         UserRepository userRepository) {
+                         UserRepository userRepository, CommentRepository commentRepository) {
         this.postService = postService;
         this.postRepository = postRepository;
         this.userRepository = userRepository;
+        this.commentRepository = commentRepository;
     }
+
+    //Post has to keep comments, not comment keep post
+    //Rewrite posts and comments DB
 
     @GetMapping("/feed")
     public String getFeedPage(Model model, Principal principal,
@@ -37,7 +46,9 @@ public class FeedContoller {
                 model.addAttribute("username", principal.getName());
                 model.addAttribute("userId", userRepository.findByUserName(principal.getName()).getId());
             }
-            model.addAttribute("allPosts", postRepository.findAll());
+            List<Post> allPosts = postRepository.findAll();
+            model.addAttribute("allPosts", allPosts);
+
         }
         return "feed";
     }
